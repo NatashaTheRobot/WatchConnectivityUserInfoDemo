fecc776f814e777dade4ff1a8edab717e44c7826
Natasha Murashev <nmurashev@gmail.com>
2015-10-21 05:58:25 -0500

    initial commit

---
+++ EmojiChat Extension/InterfaceController.swift
@@ -0,0 +1,65 @@
+//
+//  InterfaceController.swift
+//  EmojiChat Extension
+//
+//  Created by Natasha Murashev on 10/8/15.
+//  Copyright © 2015 NatashaTheRobot. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet var foodTable: WKInterfaceTable!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // remove InterfaceController as a dataSourceChangedDelegate
+        // to prevent memory leaks
+        WatchSessionManager.sharedManager.addDataSourceChangedDelegate(self)
+        loadTableData(DataSource())
+    }
+    
+    override func didDeactivate() {
+        
+        // remove InterfaceController as a dataSourceChangedDelegate
+        // to prevent memory leaks
+        WatchSessionManager.sharedManager.removeDataSourceChangedDelegate(self)
+        super.didDeactivate()
+    }
+
+}
+
+// MARK: DataSourceUpdatedDelegate
+// update the food label once the data is changed!
+extension InterfaceController: DataSourceChangedDelegate {
+    
+    func dataSourceDidUpdate(dataSource: DataSource) {
+        loadTableData(dataSource)
+    }
+}
+
+private extension InterfaceController {
+    
+    private func loadTableData(dataSource: DataSource) {
+        
+        foodTable.setNumberOfRows(dataSource.items.count, withRowType: "FoodTableRowController")
+        
+        for (index, item) in dataSource.items.enumerate() {
+            if let row = foodTable.rowControllerAtIndex(index) as? FoodTableRowController {
+                switch item {
+                case .Food(let foodItem):
+                    row.foodLabel.setText(foodItem)
+                case .Unknown:
+                    row.foodLabel.setText("¯\\_(ツ)_/¯")
+                }
+                
+            }
+        }
+        
+    }
+
+}
